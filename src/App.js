d905c24718d5ce5a0685d57e565437daf6a47a9a
Akin Evans <akinevans@gmail.com>
2024-08-03 16:59:20 -0700

    begin setup of react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Routes>{/* <Route path='' element={<Home />} /> */}</Routes>
+    </>
+  );
+};
 
 export default App;
